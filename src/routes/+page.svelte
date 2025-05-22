cfb677e3b8af2508318fa6a503a6cfe4ece017e7
Nicolai Dybdahl <nicolai.dybdahl@pasientreiser.no>
2025-05-22 10:05:34 +0200

    asdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-4xl font-bold text-center my-8 text-blue-600">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>STFU Sondre</p>
