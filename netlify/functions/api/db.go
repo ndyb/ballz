fe733f1a982dbe320a0c44c5349df683dda1af4f
Nicolai Dybdahl <nicolai.dybdahl@pasientreiser.no>
2025-05-22 11:12:22 +0200

    asdf

--- netlify/functions/api/db.go
+++ netlify/functions/api/db.go
@@ -34,7 +34,7 @@ func Connect() (*DB, error) {
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
-	connStr = "postgresql://postgres:x2XcizoJ1yD75eKr@db.wqxsyspwegwguofrewqb.supabase.co:5432/postgres"
+	//	connStr = "postgresql://postgres:x2XcizoJ1yD75eKr@db.wqxsyspwegwguofrewqb.supabase.co:5432/postgres"
 
 	log.Println("Connecting to database with connection string:", connStr)
 
