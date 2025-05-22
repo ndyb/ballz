bbb541f01b06bbc0ef650a62fa3a437478441e26
Nicolai Dybdahl <nicolai.dybdahl@pasientreiser.no>
2025-05-22 11:43:45 +0200

    asdf

--- netlify/functions/api/main.go
+++ netlify/functions/api/main.go
@@ -76,17 +76,46 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		log.Println("No route matches, returning asdf")
 
 		// Sample data for testing purposes
-		sampleData := []map[string]interface{}{
-			{"score": 393872, "created": "2025-05-22 09:37:34.987437+00"},
-			{"score": 65, "created": "2025-05-22 09:37:25.175401+00"},
-			{"score": 54, "created": "2025-05-22 09:37:19.346369+00"},
-			{"score": 43, "created": "2025-05-22 09:37:10.335945+00"},
-			{"score": 5, "created": "2025-05-21 18:26:59.365226+00"},
-			{"score": 3, "created": "2025-05-21 17:20:08.739607+00"},
-			{"score": 2, "created": "2025-05-21 16:58:29.489417+00"},
-			{"score": 1, "created": "2025-05-22 09:37:43.338297+00"},
-			{"score": 1, "created": "2025-05-21 16:47:34.136628+00"},
-		}
+		sampleData :=
+			`[
+  {
+    "score": 393872,
+    "created": "2025-05-22 09:37:34.987437+00"
+  },
+  {
+    "score": 65,
+    "created": "2025-05-22 09:37:25.175401+00"
+  },
+  {
+    "score": 54,
+    "created": "2025-05-22 09:37:19.346369+00"
+  },
+  {
+    "score": 43,
+    "created": "2025-05-22 09:37:10.335945+00"
+  },
+  {
+    "score": 5,
+    "created": "2025-05-21 18:26:59.365226+00"
+  },
+  {
+    "score": 3,
+    "created": "2025-05-21 17:20:08.739607+00"
+  },
+  {
+    "score": 2,
+    "created": "2025-05-21 16:58:29.489417+00"
+  },
+  {
+    "score": 1,
+    "created": "2025-05-22 09:37:43.338297+00"
+  },
+  {
+    "score": 1,
+    "created": "2025-05-21 16:47:34.136628+00"
+  }
+]`
+
 		return successResponse(sampleData)
 	}
 
